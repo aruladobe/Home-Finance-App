6045b0250934f5da8c31153ed94dbbada4278673
Arul kumar <arulk@adobe.com>
2026-05-19 19:35:14 +0530

    Fixed user create issue

--- server/models/User.js
+++ server/models/User.js
@@ -6,8 +6,8 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   relationship: {
     type: String,
-    enum: ['Father', 'Mother', 'Son', 'Daughter', 'Spouse', 'Grandfather', 'Grandmother', 'Other'],
-    default: 'Father'
+    enum: ['Self', 'Father', 'Mother', 'Brother', 'Sister', 'Son', 'Daughter', 'Spouse', 'Grandfather', 'Grandmother', 'Other'],
+    default: 'Self'
   },
   role: { type: String, enum: ['admin', 'user', 'manager'], default: 'admin' },
   avatar: { type: String, default: '' },
